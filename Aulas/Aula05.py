23e45ef2d3784bfa440d3cb59d33c7d94f1e9ea3
Elvis Lopes <elvislopes1996@hotmail.com>
2021-03-21 21:07:59 -0300

    aula sobre tipagem de dados em python

--- Aulas/Aula05.py
+++ Aulas/Aula05.py
@@ -2,3 +2,10 @@
 
 print('Olá mundo!')
 print('Brasil ganhou 5 titulos mundiais! Toma essa Maratona!')
+
+#declaração de variavel
+pais = 'Itália'
+quantidade = 4
+
+print(f'nome do pais é {pais} e ela ganhou {quantidade} titulos')
+type(pais)
