cff574012507a127c2404d670d065469219cd3fd
Elvis Lopes <elvislopes1996@hotmail.com>
2021-04-01 20:45:40 -0300

    feito projeto proprio sem consultar o da aula.

--- Projetos/jogo/adivinhacao.py
+++ Projetos/jogo/adivinhacao.py
@@ -1,1 +1,16 @@
+import random
+
+numero_secreto = random.randint(1, 10)
+
+print('-=' * 17)
 print('Bem-vindo ao jogo de adivinhação!')
+print('-=' * 17)
+
+numero = int(input("chuta um número de 1 a 10: "))
+
+if numero == numero_secreto:
+    print("Acertou!!!")
+if not numero == numero_secreto:
+    print("Errou")
+
+print(f'O numero da maquina é {numero_secreto}')
