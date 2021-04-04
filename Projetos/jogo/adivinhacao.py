5404dd15b6542234e72322e10d6cd0bd67f254cb
Elvis Lopes <elvislopes1996@hotmail.com>
2021-04-04 19:16:39 -0300

    Melhoria no jogo de adivinhação

--- Projetos/jogo/adivinhacao.py
+++ Projetos/jogo/adivinhacao.py
@@ -6,15 +6,17 @@ print('-=' * 17)
 print('Bem-vindo ao jogo de adivinhação!')
 print('-=' * 17)
 
-numero = int(input("chuta um número de 1 a 10: "))
+while True:
+    numero = int(input("chuta um número de 1 a 10: "))
 
-if numero == numero_secreto:
-    print("Acertou!!!")
-elif numero > numero_secreto:
-    print("Errou")
-    print("O número da maquina é menor")
-else:
-    print("Errou")
-    print("O número da maquina é maior")
+    if numero == numero_secreto:
+        print("Acertou!!!")
+        break
+    elif numero > numero_secreto:
+        print("Errou")
+        print("O número da maquina é menor")
+    else:
+        print("Errou")
+        print("O número da maquina é maior")
 
 print(f'O numero da maquina é {numero_secreto}')
